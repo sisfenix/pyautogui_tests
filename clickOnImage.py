547801daf32d62cd5de4b1bb53c2d4a17455cee4
Alan Lopes <desenv.alanlopes@gmail.com>
2024-03-05 16:47:01 -0300

    chg: funcao para cancelar o script apos combinacao de teclas

--- clickOnImage.py
+++ clickOnImage.py
@@ -1,5 +1,12 @@
 import pyautogui
+import keyboard
 import time
+
+def check_for_exit_combination():
+    # Verifica se a combinação Ctrl + Alt + C foi pressionada para sair do script.
+    if keyboard.is_pressed('ctrl') and keyboard.is_pressed('alt') and keyboard.is_pressed('c'):
+        print("Combinação de saída detectada, terminando o script.")
+        exit()
 
 def locate_image():
     cords_image = pyautogui.locateOnScreen('nao.png', confidence=0.7)
@@ -10,6 +17,7 @@ def locate_image():
 
 def locate_image_loop():
     while True:
+        check_for_exit_combination()
         cords_image = pyautogui.locateOnScreen('images/nao.png', confidence=0.7)
         if cords_image is not None:
             print(cords_image)
